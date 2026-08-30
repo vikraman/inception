446c1fce4593acb9a1f81e0343268a3cfb3df479
Gregor Feierabend <gf.devel@pm.me>
2026-08-30 18:41:16 +0100

    updated index script to account for lagda files

--- index.sh
+++ index.sh
@@ -3,7 +3,7 @@
 set -eu
 set -o pipefail
 
-for file in $( find -H Inception -type f -name '*.agda' | sort ); do
+for file in $( find -H Inception -type f -name '*.agda' -o -name '*.lagda' | sort ); do
     i=$( echo ${file} | sed 's/Inception\/\(.*\)\.agda/Inception\/\1/' | sed 's/\//\./g' )
     echo "import ${i}" >> index.agda
 done
