e70d7511d73fcff0e22f7759ecf0a97afc8be75e
Gregor Feierabend <gf.devel@pm.me>
2026-09-25 15:21:18 +0100

    added lagda_to_agda.sh to strip lagda files of LaTeX code before exporting to html

--- index.sh
+++ index.sh
@@ -3,7 +3,10 @@
 set -eu
 set -o pipefail
 
-for file in $( find -H Inception -type f -name '*.agda' -o -name '*.lagda' | sort ); do
+./lagda_to_agda.sh $( find -H Inception -type f -name '*.lagda')
+find -H Inception -type f -name '*.lagda' -exec rm {} \;
+
+for file in $( find -H Inception -type f -name '*.agda' | sort ); do
     i=$( echo ${file} | sed 's/Inception\/\(.*\)\.l\?agda/Inception\/\1/' | sed 's/\//\./g' )
     echo "import ${i}" >> index.agda
 done
